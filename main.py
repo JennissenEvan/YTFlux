7a7b9173e974caf7e0e59e9528552fc315690f14
JennissenEvan <jennissenevan@gmail.com>
2022-05-07 04:53:36 -0500

    add database initialization

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import mutagen
 from pytube import YouTube, Playlist
 
 
+FILENAME_FORMAT = "'[' || SUBSTR('00000' || CAST({num} AS TEXT), -5, 5) || ']' || ' ' || {title} || '.mp4'"
+
+
 def run():
     db = sqlite3.connect("playlist.db")
 
@@ -14,6 +17,24 @@ def run():
         c = db.cursor()
         c.execute(q)
         return c.fetchall()
+
+    execute_query("""
+CREATE TABLE IF NOT EXISTS Playlist (
+    num INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    vid_id CHAR(11) NOT NULL,
+    title NTEXT NULL,
+    is_downloaded BOOLEAN NOT NULL DEFAULT FALSE,
+    is_available BOOLEAN NULL
+);
+""")
+
+    execute_query(f"""
+    CREATE VIEW IF NOT EXISTS PlaylistExpanded AS
+    SELECT
+        *, 
+        {FILENAME_FORMAT.format(num="num", title="title")} AS song_file_name 
+    FROM Playlist;
+    """)
 
 
 if __name__ == "__main__":
