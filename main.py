fd70e94da6dd2c530770c258d76f2691689c8453
JennissenEvan <jennissenevan@gmail.com>
2022-05-07 04:47:21 -0500

    add base database access

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ from pytube import YouTube, Playlist
 
 
 def run():
-    pass
+    db = sqlite3.connect("playlist.db")
+
+    def execute_query(q: str):
+        db.cursor().execute(q)
+        db.commit()
+
+    def fetch_query(q: str) -> list[tuple[...]]:
+        c = db.cursor()
+        c.execute(q)
+        return c.fetchall()
 
 
 if __name__ == "__main__":
